4a4243c78faf2299a2b6a2f08425b92b25c34ade
sdfee112 <162119699+sdfee112@users.noreply.github.com>
2024-07-06 21:36:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const os = require("os");
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;
 
 
-console.log(==============================);
+console.log(`==============================`);
 console.log(``);
-console.log("     /stas 查看进程");
-console.log("     /listen 查看端口");
+console.log("     /stas View processes");
+console.log("     /listen Check ports");
 console.log("     /start 手动启动脚本");
 console.log("     /res 手动恢复");
 console.log("     /backup 手动备份");
 console.log(``);
-console.log(==============================);
+console.log(`==============================`);
 
 app.get("/", function (req, res) {
   res.send("hello world");
@@ -60,6 +60,7 @@ app.get("/listen", function (req, res) {
   });
 });
 
+
 app.get("/start", (req, res) => {
   const startScript = exec("sh /dashboard/init.sh");
 
@@ -68,11 +69,11 @@ app.get("/start", (req, res) => {
   });
 
   startScript.stderr.on("data", (data) => {
-    res.write(stderr: ${data});
+    res.write(`stderr: ${data}`);
   });
 
   startScript.on("close", (code) => {
-    res.write(Child process exited with code ${code});
+    res.write(`Child process exited with code ${code}`);
     res.end();
   });
 });
@@ -85,11 +86,11 @@ app.get("/res", (req, res) => {
   });
 
   resScript.stderr.on("data", (data) => {
-    res.write(stderr: ${data});
+    res.write(`stderr: ${data}`);
   });
 
   resScript.on("close", (code) => {
-    res.write(Child process exited with code ${code});
+    res.write(`Child process exited with code ${code}`);
     res.end();
   });
 });
@@ -102,16 +103,16 @@ app.get("/backup", (req, res) => {
   });
 
   backupScript.stderr.on("data", (data) => {
-    res.write(stderr: ${data});
+    res.write(`stderr: ${data}`);
   });
 
   backupScript.on("close", (code) => {
-    res.write(Child process exited with code ${code});
+    res.write(`Child process exited with code ${code}`);
     res.end();
   });
 });
 
-// 自动启动init.sh脚本
+// 自动启动 start.sh 脚本
 const autoStartScript = exec("sh /dashboard/init.sh");
 
 autoStartScript.stdout.on("data", (data) => {
@@ -119,13 +120,13 @@ autoStartScript.stdout.on("data", (data) => {
 });
 
 autoStartScript.stderr.on("data", (data) => {
-  console.error(stderr: ${data});
+  console.error(`stderr: ${data}`);
 });
 
 autoStartScript.on("close", (code) => {
-  console.log(Child process exited with code ${code});
+  console.log(`Child process exited with code ${code}`);
 });
 
 app.listen(port, () => {
-  console.log(Example app listening on port ${port}!\n==============================);
+  console.log(`Example app listening on port ${port}!\n==============================`);
 });
